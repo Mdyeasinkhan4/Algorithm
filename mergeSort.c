9ab0965d2ef5bd97f049a31c0ad555de7e3aefe6
Md Yeasin khan <87030606+Mdyeasinkhan4@users.noreply.github.com>
2025-05-01 20:49:03 +0600

    Add files via upload

---
+++ mergeSort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mergesort();
+void merge();
+int main(){
+    int a[50], n, i;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    printf("Enter Elements: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    mergesort(a,0,n-1);
+    printf("Sorted Elements are: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+
+}
+
+void mergesort(int a[], int first, int last)
+{
+    int mid;
+    if(first<last)
+    {
+        mid = (first+last)/2;
+        mergesort(a,first,mid);
+        mergesort(a, mid+1,last);
+        merge(a, first,mid,last);
+    }
+}
+
+void merge(int a[], int first, int mid, int last)
+{
+    int b[50];
+    int i,j,k;
+    i=first;
+    j=mid+1;
+    k=first;
+    while (i<=mid && j<=last)
+    {
+        if(a[i]<=a[j])
+            b[k++]=a[i++];
+        else
+            b[k++]=a[j++];
+    }
+    if(i>mid)
+    {
+        while(j<=last)
+            b[k++]=a[j++];
+    }
+    else
+    {
+        while(i<=mid)
+            b[k++]=a[i++];
+    }
+    for(i=first; i<=last; i++)
+        a[i]=b[i];
+}
